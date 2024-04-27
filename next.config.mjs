a04aadf623ffbe5dbdeb2333fa1425b0b3177140
Alexander Kovzan <alexanderkovzan@gmail.com>
2024-04-27 12:39:22 +0300

    Add movie api proxy (#4)
    * feat: add all movie proxy endpoint
    
    * chore: update gitignore
    
    * feat: add genres proxy endpoint
    
    * feat: add single movie proxy endpoint
    
    * fix: fix build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/discover/movie",
+        destination: `${process.env.BASE_API_URL}/discover/movie?api_key=${process.env.API_KEY}`,
+      },
+      {
+        source: "/genre/movie/list",
+        destination: `${process.env.BASE_API_URL}/genre/movie/list?api_key=${process.env.API_KEY}`,
+      },
+      {
+        source: "/movie/:movieId",
+        destination: `${process.env.BASE_API_URL}/movie/:movieId?api_key=${process.env.API_KEY}&append_to_response=videos`,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
