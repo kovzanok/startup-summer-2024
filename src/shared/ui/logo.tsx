b7b7b2f5409e510407d7a4528411f98bc7e5944e
Alex Kovzan <alexanderkovzan@gmail.com>
2024-04-27 12:46:41 +0300

    feat: change svg logo import

--- src/shared/ui/logo.tsx
+++ src/shared/ui/logo.tsx
@@ -1,16 +1,15 @@
 "use client";
 
 import { Flex, Title, useMantineTheme } from "@mantine/core";
-import Image from "next/image";
 import Link from "next/link";
 
-import logoSrc from "@/../public/logo.svg";
+import LogoIcon from "@/../public/logo.svg";
 
 export function Logo() {
   const theme = useMantineTheme();
   return (
     <Flex href="/" component={Link} td="none" columnGap={12}>
-      <Image src={logoSrc} alt="Logo" />
+      <LogoIcon />
       <Title c={theme.colors.purple[2]} size={24} order={1}>
         ArrowFlicks
       </Title>
