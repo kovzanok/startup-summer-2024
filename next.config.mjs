4ee23524f161311a1a8edd65fa5e2e276573cb2a
Alex Kovzan <alexanderkovzan@gmail.com>
2024-04-27 12:20:12 +0300

    feat: add all movie proxy endpoint

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/discover/movie",
+        destination: `${process.env.BASE_API_URL}/discover/movie?api_key=${process.env.API_KEY}`,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
