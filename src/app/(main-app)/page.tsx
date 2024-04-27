11db38b28180573510725f8a5b3e3f8aca9e74a3
Alex Kovzan <alexanderkovzan@gmail.com>
2024-04-27 22:22:15 +0300

    feat: add movie list to main page

--- src/app/(main-app)/page.tsx
+++ src/app/(main-app)/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+
+import { Box, useMantineTheme } from "@mantine/core";
+import useSWR from "swr";
+
+import { fetcher } from "@/shared/lib";
+import { DiscoverMovieRes } from "@/shared/types";
+import { MovieList } from "@/widgets/movie-list";
+
 export default function Home() {
-  return <main>Home</main>;
+  const { data: movieList, isLoading } = useSWR<DiscoverMovieRes>(
+    "/discover/movie",
+    fetcher,
+  );
+
+  const theme = useMantineTheme();
+  return (
+    <Box px={90} bg={theme.colors.gray[0]}>
+      <MovieList movieList={movieList?.results} isLoading={isLoading} />
+    </Box>
+  );
 }
