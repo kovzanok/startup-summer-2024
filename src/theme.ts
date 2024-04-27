85d0557137c6bceb7b5b1567742a8b42e03be979
Alex Kovzan <alexanderkovzan@gmail.com>
2024-04-27 10:32:06 +0300

    feat: add color to theme object

--- src/theme.ts
+++ src/theme.ts
@@ -3,6 +3,16 @@ import { colorsTuple, createTheme } from "@mantine/core";
 export const theme = createTheme({
   colors: {
     purple: colorsTuple(["#F2EBF9", "#E5D5FA", "#9854F6"]),
+    gray: colorsTuple(["#F5F5F6"]),
+    notFound: colorsTuple([
+      "#FFFFFF",
+      "#FBE54D",
+      "#74FADB",
+      "#68DC42",
+      "#E83CF2",
+      "#D52D25",
+      "#0732B3",
+    ]),
   },
   black: "#000000",
 });
