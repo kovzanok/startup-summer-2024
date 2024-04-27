20f25824be7212ae7097acd80c48405641abc50b
Alex Kovzan <alexanderkovzan@gmail.com>
2024-04-27 12:32:24 +0300

    feat: add single movie proxy endpoint

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
         source: "/genre/movie/list",
         destination: `${process.env.BASE_API_URL}/genre/movie/list?api_key=${process.env.API_KEY}`,
       },
+      {
+        source: "/movie/:movieId",
+        destination: `${process.env.BASE_API_URL}/movie/:movieId?api_key=${process.env.API_KEY}&append_to_response=videos`,
+      },
     ];
   },
   },
