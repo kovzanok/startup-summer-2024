a5fea4a40453e6430f39a35abfe46f784d75c46f
Alex Kovzan <alexanderkovzan@gmail.com>
2024-04-28 09:25:51 +0300

    feat: add search params to api query

--- src/app/(main-app)/page.tsx
+++ src/app/(main-app)/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Box, useMantineTheme } from "@mantine/core";
+import { useSearchParams } from "next/navigation";
 import useSWR from "swr";
 
 import { fetcher } from "@/shared/lib";
@@ -8,15 +9,20 @@ import { DiscoverMovieRes } from "@/shared/types";
 import { MovieList } from "@/widgets/movie-list";
 
 export default function Home() {
-  const { data: movieList, isLoading } = useSWR<DiscoverMovieRes>(
-    "/discover/movie",
+  const searchParams = useSearchParams();
+  const { data: movieRes, isLoading } = useSWR<DiscoverMovieRes>(
+    `/discover/movie?${searchParams.toString()}`,
     fetcher,
   );
 
   const theme = useMantineTheme();
   return (
     <Box px={90} bg={theme.colors.gray[0]}>
-      <MovieList movieList={movieList?.results} isLoading={isLoading} />
+      <MovieList
+        total={movieRes?.total_pages}
+        movieList={movieRes?.results}
+        isLoading={isLoading}
+      />
     </Box>
   );
 }
