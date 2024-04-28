30f89e9108684a8c5030c97fbb5fbe93979a24bd
Alex Kovzan <alexanderkovzan@gmail.com>
2024-04-28 12:18:38 +0300

    feat: add sort select component to main page

---
+++ src/app/(main-app)/_component/sort-select.tsx
@@ -0,0 +1,46 @@
+import { Box } from "@mantine/core";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+
+import { createQueryString } from "@/shared/lib";
+import { CustomSelect, SelectOptionType } from "@/shared/ui";
+
+const options: SelectOptionType[] = [
+  { value: "popularity.asc", name: "Least Popular" },
+  { value: "popularity.desc", name: "Most Popular" },
+  { value: "revenue.asc", name: "Lowest Revenue" },
+  { value: "revenue.desc", name: "Highest Revenue" },
+  { value: "primary_release_date.asc", name: "Latest Release Date" },
+  { value: "primary_release_date.desc", name: "Earliest Release Date" },
+  { value: "title.asc", name: "Title (A-Z)" },
+  { value: "title.desc", name: "Title (Z-A)" },
+  { value: "vote_average.asc", name: "Least Rated" },
+  { value: "vote_average.desc", name: "Most Rated" },
+  { value: "vote_count.asc", name: "Least Voted" },
+  { value: "vote_count.desc", name: "Most Voted" },
+];
+
+export function SortSelect() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const handleChange = (value: string) => {
+    router.push(
+      `${pathname}?${createQueryString(searchParams, "sort_by", value)}`,
+    );
+  };
+  return (
+    <Box mb={24} ml="auto" maw={284}>
+      <CustomSelect
+        initialValue={
+          options.find(o => o.value === searchParams.get("sort_by"))
+            ? searchParams.get("sort_by")
+            : "popularity.desc"
+        }
+        onChange={handleChange}
+        options={options}
+        label="Sort by"
+      />
+    </Box>
+  );
+}
--- src/app/(main-app)/page.tsx
+++ src/app/(main-app)/page.tsx
@@ -4,12 +4,14 @@ import { Box, useMantineTheme } from "@mantine/core";
 import { Suspense } from "react";
 
 import { DiscoverMovieList } from "./_component/discover-movie-list";
+import { SortSelect } from "./_component/sort-select";
 
 export default function Home() {
   const theme = useMantineTheme();
   return (
     <Box px={90} bg={theme.colors.gray[0]}>
       <Suspense>
+        <SortSelect />
         <DiscoverMovieList />
       </Suspense>
     </Box>
