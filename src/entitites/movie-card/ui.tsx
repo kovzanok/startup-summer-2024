8e21f900e8ffffa2a7f50b804eff61c926d9a131
Alex Kovzan <alexanderkovzan@gmail.com>
2024-04-30 19:05:05 +0300

    feat: add link to movie card

--- src/entitites/movie-card/ui.tsx
+++ src/entitites/movie-card/ui.tsx
@@ -8,6 +8,7 @@ import {
   useMantineTheme,
 } from "@mantine/core";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 import posterPlaceholderSrc from "@/../public/poster-placeholder.png";
@@ -20,6 +21,7 @@ type MovieCardProps = Movie & {
 };
 
 export function MovieCard({
+  id,
   genre_ids,
   poster_path,
   title,
@@ -31,62 +33,64 @@ export function MovieCard({
 }: MovieCardProps) {
   const theme = useMantineTheme();
   return (
-    <Card p={24}>
-      <Flex columnGap={16}>
-        <Image
-          width={120}
-          height={170}
-          src={
-            poster_path
-              ? `https://image.tmdb.org/t/p/w500${poster_path}`
-              : posterPlaceholderSrc
-          }
-          alt={title}
-        />
-        <Flex maw={263} flex={1} direction="column" rowGap={8}>
-          <Title
-            order={3}
-            fz={20}
-            lh="24px"
-            fw={600}
-            c={theme.colors.purple[2]}
-          >
-            {title}
-          </Title>
-          <Text lh="20px" c={theme.colors.gray[1]}>
-            {new Date(release_date).getFullYear()}
-          </Text>
-          <Flex align="center" columnGap={4}>
-            <StartIcon color={theme.colors.yellow[0]} />
-            <Text fw={600} lh="20px">
-              {vote_average.toFixed(1)}
+    <Link style={{ textDecoration: "none" }} href={`/${id}`}>
+      <Card p={24}>
+        <Flex columnGap={16}>
+          <Image
+            width={120}
+            height={170}
+            src={
+              poster_path
+                ? `https://image.tmdb.org/t/p/w500${poster_path}`
+                : posterPlaceholderSrc
+            }
+            alt={title}
+          />
+          <Flex maw={263} flex={1} direction="column" rowGap={8}>
+            <Title
+              order={3}
+              fz={20}
+              lh="24px"
+              fw={600}
+              c={theme.colors.purple[2]}
+            >
+              {title}
+            </Title>
+            <Text lh="20px" c={theme.colors.gray[1]}>
+              {new Date(release_date).getFullYear()}
             </Text>
-            <Text c={theme.colors.gray[1]} ml={4}>
-              ({vote_count})
-            </Text>
+            <Flex align="center" columnGap={4}>
+              <StartIcon color={theme.colors.yellow[0]} />
+              <Text fw={600} lh="20px">
+                {vote_average.toFixed(1)}
+              </Text>
+              <Text c={theme.colors.gray[1]} ml={4}>
+                ({vote_count})
+              </Text>
+            </Flex>
+            <Flex mt="auto" columnGap={8} wrap="wrap">
+              <Text c={theme.colors.gray[1]}>Genres</Text>
+              <Text>
+                {isGenresLoading && (
+                  <Flex columnGap={4}>
+                    <Skeleton width="70px" h={25} />
+                    <Skeleton width="50px" h={25} />
+                    <Skeleton width="60px" h={25} />
+                  </Flex>
+                )}
+                {genres
+                  ?.filter(g => genre_ids.includes(g.id))
+                  .slice(0, 3)
+                  .map(({ name }) => name)
+                  .join(", ")}
+              </Text>
+            </Flex>
           </Flex>
-          <Flex mt="auto" columnGap={8} wrap="wrap">
-            <Text c={theme.colors.gray[1]}>Genres</Text>
-            <Text>
-              {isGenresLoading && (
-                <Flex columnGap={4}>
-                  <Skeleton width="70px" h={25} />
-                  <Skeleton width="50px" h={25} />
-                  <Skeleton width="60px" h={25} />
-                </Flex>
-              )}
-              {genres
-                ?.filter(({ id }) => genre_ids.includes(id))
-                .slice(0, 3)
-                .map(({ name }) => name)
-                .join(", ")}
-            </Text>
-          </Flex>
+          <ActionIcon ml="auto" variant="transparent">
+            <StartIcon color={theme.colors.gray[2]} />
+          </ActionIcon>
         </Flex>
-        <ActionIcon ml="auto" variant="transparent">
-          <StartIcon color={theme.colors.gray[2]} />
-        </ActionIcon>
-      </Flex>
-    </Card>
+      </Card>
+    </Link>
   );
 }
