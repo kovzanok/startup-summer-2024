e1c346160aa98419a12d91fbf9cfaa15569b88bc
Alex Kovzan <alexanderkovzan@gmail.com>
2024-05-01 10:26:52 +0300

    refactor: refactor movie card component

--- src/entitites/movie-card/ui.tsx
+++ src/entitites/movie-card/ui.tsx
@@ -2,18 +2,19 @@ import {
   ActionIcon,
   Card,
   Flex,
+  Image,
   Skeleton,
   Text,
-  Title,
   useMantineTheme,
 } from "@mantine/core";
-import Image from "next/image";
+import NextImage from "next/image";
 import Link from "next/link";
 import React from "react";
 
-import posterPlaceholderSrc from "@/../public/poster-placeholder.png";
+import posterPlaceholder from "@/../public/poster-placeholder.png";
 import StartIcon from "@/../public/star.svg";
 import { Genre, Movie } from "@/shared/types";
+import { MovieInfo } from "@/shared/ui";
 
 type MovieCardProps = Movie & {
   genres: Genre[] | undefined;
@@ -37,53 +38,41 @@ export function MovieCard({
       <Card p={24}>
         <Flex columnGap={16}>
           <Image
+            style={{ border: `1px solid ${theme.colors.gray[3]}` }}
+            component={NextImage}
+            w={120}
             width={120}
+            h={170}
             height={170}
-            src={
-              poster_path
-                ? `https://image.tmdb.org/t/p/w500${poster_path}`
-                : posterPlaceholderSrc
-            }
+            src={`${process.env.NEXT_PUBLIC_IMAGE_SRC}/w185${poster_path}`}
+            fallbackSrc={posterPlaceholder.src}
             alt={title}
+            priority
           />
-          <Flex maw={263} flex={1} direction="column" rowGap={8}>
-            <Title
-              order={3}
-              fz={20}
-              lh="24px"
-              fw={600}
-              c={theme.colors.purple[2]}
-            >
-              {title}
-            </Title>
-            <Text lh="20px" c={theme.colors.gray[1]}>
-              {new Date(release_date).getFullYear()}
-            </Text>
-            <Flex align="center" columnGap={4}>
-              <StartIcon color={theme.colors.yellow[0]} />
-              <Text fw={600} lh="20px">
-                {vote_average.toFixed(1)}
-              </Text>
-              <Text c={theme.colors.gray[1]} ml={4}>
-                ({vote_count})
-              </Text>
-            </Flex>
-            <Flex mt="auto" columnGap={8} wrap="wrap">
+          <Flex maw={263} flex={1} direction="column" justify="space-between">
+            <MovieInfo
+              title={title}
+              release_date={release_date}
+              vote_average={vote_average}
+              vote_count={vote_count}
+            />
+            <Flex columnGap={8} wrap="wrap">
               <Text c={theme.colors.gray[1]}>Genres</Text>
-              <Text>
-                {isGenresLoading && (
-                  <Flex columnGap={4}>
-                    <Skeleton width="70px" h={25} />
-                    <Skeleton width="50px" h={25} />
-                    <Skeleton width="60px" h={25} />
-                  </Flex>
-                )}
-                {genres
-                  ?.filter(g => genre_ids.includes(g.id))
-                  .slice(0, 3)
-                  .map(({ name }) => name)
-                  .join(", ")}
-              </Text>
+              {isGenresLoading ? (
+                <Flex columnGap={4}>
+                  <Skeleton width="70px" h={25} />
+                  <Skeleton width="50px" h={25} />
+                  <Skeleton width="60px" h={25} />
+                </Flex>
+              ) : (
+                <Text>
+                  {genres
+                    ?.filter(g => genre_ids.includes(g.id))
+                    .slice(0, 3)
+                    .map(({ name }) => name)
+                    .join(", ")}
+                </Text>
+              )}
             </Flex>
           </Flex>
           <ActionIcon ml="auto" variant="transparent">
