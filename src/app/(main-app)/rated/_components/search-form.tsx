dfcedd5a61054fb4e08a19171008eb4b25ae622d
Alexander Kovzan <alexanderkovzan@gmail.com>
2024-05-02 13:37:42 +0300

    Add rated movie search (#12)
    * feat: change pagination position based on type list
    
    * feat: add search form
    
    * feat: filter movies based on search
    
    * fix: fix infinite page reduction

---
+++ src/app/(main-app)/rated/_components/search-form.tsx
@@ -0,0 +1,46 @@
+import { Box, Button, Input, useMantineTheme } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { useSearchParams } from "next/navigation";
+
+import SearchIcon from "@/../public/search.svg";
+import { useUpdateSearchParams } from "@/shared/hooks";
+
+type FormValue = { search: string };
+
+export function SearchForm() {
+  const theme = useMantineTheme();
+  const updateSearchParms = useUpdateSearchParams();
+  const searchParams = useSearchParams();
+  const { onSubmit, getInputProps } = useForm<FormValue>({
+    initialValues: { search: searchParams.get("search") || "" },
+  });
+  const handleSubmit = ({ search }: FormValue) => {
+    updateSearchParms("search", search);
+  };
+  return (
+    <Box maw="50%" w="100%">
+      <form onSubmit={onSubmit(handleSubmit)}>
+        <Input
+          {...getInputProps("search")}
+          radius={8}
+          styles={{ input: { padding: "24px 0", paddingLeft: "32px" } }}
+          leftSection={<SearchIcon />}
+          rightSection={
+            <Button
+              type="submit"
+              radius={8}
+              bg={theme.colors.purple[2]}
+              px={20}
+              py={6}
+            >
+              Search
+            </Button>
+          }
+          rightSectionPointerEvents="all"
+          rightSectionWidth={100}
+          placeholder="Search movie title"
+        />
+      </form>
+    </Box>
+  );
+}
