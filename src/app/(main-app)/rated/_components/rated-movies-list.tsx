4ab2474b3dc84a37c220605fd229b304445d2d89
Alex Kovzan <alexanderkovzan@gmail.com>
2024-05-02 10:13:07 +0300

    feat: handle list blink after item remove

--- src/app/(main-app)/rated/_components/rated-movies-list.tsx
+++ src/app/(main-app)/rated/_components/rated-movies-list.tsx
@@ -1,12 +1,15 @@
 import { useSearchParams } from "next/navigation";
-import { useContext, useMemo } from "react";
+import { useContext, useEffect, useMemo, useState } from "react";
 
 import { RatingContext, RatingContextValue } from "@/shared/context";
+import { useUpdateSearchParams } from "@/shared/hooks";
 import { chunk } from "@/shared/lib";
 import { MovieList } from "@/widgets/movie-list";
 
 export function RatedMoviesList() {
   const searchParams = useSearchParams();
+  const [isPageLoading, setIsPageLoading] = useState(false);
+  const updateSearchParams = useUpdateSearchParams();
   const page = useMemo(
     () => Number(searchParams.get("page")) || 1,
     [searchParams],
@@ -15,12 +18,22 @@ export function RatedMoviesList() {
     RatingContext,
   ) as RatingContextValue;
   const chunkedMovieList = useMemo(() => chunk(ratedMovies, 4), [ratedMovies]);
+  useEffect(() => {
+    if (isPageLoading) {
+      setIsPageLoading(false);
+    }
+    if (!isLoading && !chunkedMovieList[page - 1]) {
+      updateSearchParams("page", page - 1);
+      setIsPageLoading(true);
+    }
+  }, [chunkedMovieList, page, isLoading, setIsPageLoading, isPageLoading]);
+
   return (
     <MovieList
       rateMovie={rateMovie}
       ratedMovies={ratedMovies}
       total={Math.ceil(ratedMovies.length / 4)}
-      isLoading={isLoading}
+      isLoading={isLoading || isPageLoading}
       movieList={chunkedMovieList[page - 1]}
       isRatingList
     />
