b3a2ed0ced9c8d127be2c98ea2f1f4513af0d9a0
Alex Kovzan <alexanderkovzan@gmail.com>
2024-05-02 13:08:29 +0300

    feat: add search form

--- src/app/(main-app)/rated/page.tsx
+++ src/app/(main-app)/rated/page.tsx
@@ -1,14 +1,21 @@
 "use client";
 
-import { Box } from "@mantine/core";
+import { Box, Flex, Title } from "@mantine/core";
 import { Suspense } from "react";
 
 import { RatedMoviesList } from "./_components/rated-movies-list";
+import { SearchForm } from "./_components/search-form";
 
 export default function RatedMoviesPage() {
   return (
     <Box px={90} py={40}>
       <Suspense>
+        <Flex mb={40} justify="space-between">
+          <Title order={2} fz={32}>
+            Rated movies
+          </Title>
+          <SearchForm />
+        </Flex>
         <RatedMoviesList />
       </Suspense>
     </Box>
